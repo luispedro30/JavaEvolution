22d4774dfd91372f09ec974206e0c38b823d69dc
LUÍS PEDRO MAGALHÃES DA SILVA <8220025@estg.ipp.pt>
2023-07-28 14:34:36 +0100

    Change Student Class

--- MotorBikeClass/src/main/java/org/example/Student.java
+++ MotorBikeClass/src/main/java/org/example/Student.java
@@ -1,45 +1,48 @@
 package org.example;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Student {
 
     private String name;
-    private int[] marks;
+    private ArrayList<Integer> marks = new ArrayList<Integer>();
 
     public Student (String nameInput, int[] marksInput){
         this.name = nameInput;
-        this.marks  = marksInput;
+        for (int mark:marksInput){
+            this.marks.add(mark);
+        }
     };
 
     public int getNumberOfMarks(){
-        return this.marks.length;
+        return this.marks.size();
     };
 
     public int getTotalSumOfMarks(){
         int sum = 0;
-        for (int i = 0; i <= this.marks.length -1 ; i++){
-            sum = sum + this.marks[i];
+        for (int mark: this.marks){
+            sum = sum + mark;
         }
         return sum;
     };
 
     public int getMaximumMarks(){
-        int max = this.marks[0];
-        for (int i = 0; i <= this.marks.length -1 ; i++){
-            if (this.marks[i] > max){
-                max = this.marks[i];
+        int max = 0;
+        for (int mark: this.marks){
+            if (mark > max){
+                max = mark;
             }
         }
         return max;
     }
 
     public int getMinimumMarks(){
-        int min = this.marks[0];
-        for (int i = 0; i <= this.marks.length -1 ; i++){
-            if (this.marks[i] < min){
-                min = this.marks[i];
+        int min = this.marks.get(0);
+        for (int mark: this.marks){
+            if (mark < min){
+                min = mark;
             }
         }
         return min;
@@ -48,10 +51,10 @@ public class Student {
     public float getAverageMarks(){
         float sum = 0;
         float mean = 0;
-        for (int i = 0; i <= this.marks.length -1 ; i++){
-            sum = sum + this.marks[i];
+        for (int mark: this.marks){
+            sum = sum + mark;
         }
-        mean = sum / this.marks.length;
+        mean = sum / this.marks.size();
         return mean;
     };
 
