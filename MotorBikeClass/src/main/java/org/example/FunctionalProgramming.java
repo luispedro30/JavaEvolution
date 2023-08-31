b29ba71da0738714daaa6edd952cd911a7e08505
LUÍS PEDRO MAGALHÃES DA SILVA <8220025@estg.ipp.pt>
2023-08-31 11:47:11 +0100

    <feat: usage of filtering>

--- MotorBikeClass/src/main/java/org/example/FunctionalProgramming.java
+++ MotorBikeClass/src/main/java/org/example/FunctionalProgramming.java
@@ -1,2 +1,29 @@
-package org.example;public class FunctionalProgramming {
+package org.example;
+
+import java.util.List;
+
+public class FunctionalProgramming <T>{
+    public static void main(String[] args) {
+        List<String> list = List.of("Apple","Banana","Cat","Dog");
+        List<Integer> listNumbers = List.of(1,4,7,9);
+        printBasicWithFpI(listNumbers);
+        printBasicWithFpFiltering(list);
+    }
+
+    private static void printBasic(List<String> list){
+        for (String string:list){
+            System.out.println(string);
+        }
+    }
+
+    public static void printBasicWithFp(List<String> list){
+        list.stream().forEach(element -> System.out.println(element));
+    }
+
+    public static void printBasicWithFpFiltering(List<String> list){
+        list.stream().filter(element -> element.endsWith("at")).forEach(element -> System.out.println(element));
+    }
+    public static void printBasicWithFpI(List<Integer> list){
+        list.stream().forEach(element -> System.out.println(element));
+    }
 }
